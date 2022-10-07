88703adc80255893eb48a4b9dd063c4139f3e113
Ahmed Nabil <a7med.nabil1655@gmail.com>
2022-10-07 21:39:44 +0200

    Surveys page, create survey form with questions in Vue

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -5,18 +5,27 @@
 				<div class="flex h-16 items-center justify-between">
 					<div class="flex items-center">
 						<div class="flex-shrink-0">
-							<img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-								alt="Your Company" />
+							<img
+								class="h-8 w-8"
+								src="https://picsum.photos/200"
+								alt="Your Company"
+							/>
 						</div>
 						<div class="hidden md:block">
 							<div class="ml-10 flex items-baseline space-x-4">
-								<router-link v-for="item in navigation" :key="item.name" :to="item.to"
-									active-class="bg-gray-900 text-white" :class="[
+								<router-link
+									v-for="item in navigation"
+									:key="item.name"
+									:to="item.to"
+									active-class="bg-gray-900 text-white"
+									:class="[
 										this.$route.name === item.to.name
 											? ''
 											: 'text-gray-300 hover:bg-gray-700 hover:text-white',
 										'px-3 py-2 rounded-md text-sm font-medium',
-									]">{{ item.name }}</router-link>
+									]"
+									>{{ item.name }}</router-link
+								>
 							</div>
 						</div>
 					</div>
@@ -26,21 +35,35 @@
 							<Menu as="div" class="relative ml-3">
 								<div>
 									<MenuButton
-										class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+										class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+									>
 										<span class="sr-only">Open user menu</span>
-										<img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+										<img
+											class="h-8 w-8 rounded-full"
+											:src="user.imageUrl ?? 'https://picsum.photos/200'"
+											alt=""
+										/>
 									</MenuButton>
 								</div>
-								<transition enter-active-class="transition ease-out duration-100"
-									enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-									leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
-									leave-to-class="transform opacity-0 scale-95">
+								<transition
+									enter-active-class="transition ease-out duration-100"
+									enter-from-class="transform opacity-0 scale-95"
+									enter-to-class="transform opacity-100 scale-100"
+									leave-active-class="transition ease-in duration-75"
+									leave-from-class="transform opacity-100 scale-100"
+									leave-to-class="transform opacity-0 scale-95"
+								>
 									<MenuItems
-										class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+										class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+									>
 										<MenuItem>
-										<a @click="logout" :class="[
-											'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
-										]">Logout</a>
+											<a
+												@click="logout"
+												:class="[
+													'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
+												]"
+												>Logout</a
+											>
 										</MenuItem>
 									</MenuItems>
 								</transition>
@@ -50,9 +73,14 @@
 					<div class="-mr-2 flex md:hidden">
 						<!-- Mobile menu button -->
 						<DisclosureButton
-							class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+							class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+						>
 							<span class="sr-only">Open main menu</span>
-							<Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+							<Bars3Icon
+								v-if="!open"
+								class="block h-6 w-6"
+								aria-hidden="true"
+							/>
 							<XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
 						</DisclosureButton>
 					</div>
@@ -60,28 +88,47 @@
 			</div>
 			<DisclosurePanel class="md:hidden">
 				<div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-					<router-link v-for="item in navigation" :key="item.name" as="a" :to="item.to"
-						active-class="bg-gray-900 text-white" :class="[
+					<router-link
+						v-for="item in navigation"
+						:key="item.name"
+						as="a"
+						:to="item.to"
+						active-class="bg-gray-900 text-white"
+						:class="[
 							this.$route.name === item.to.name
 								? ''
 								: 'text-gray-300 hover:bg-gray-700 hover:text-white',
 							'block px-3 py-2 rounded-md text-base font-medium',
-						]">{{ item.name }}</router-link>
+						]"
+						>{{ item.name }}</router-link
+					>
 				</div>
 				<div class="border-t border-gray-700 pt-4 pb-3">
 					<div class="flex items-center px-5">
 						<div class="flex-shrink-0">
-							<img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+							<img
+								class="h-10 w-10 rounded-full"
+								:src="user.imageUrl ?? 'https://picsum.photos/200'"
+								alt=""
+							/>
 						</div>
 						<div class="ml-3">
-							<div class="text-base font-medium leading-none text-white"> {{ user.name }} </div>
-							<div class="text-sm font-medium leading-none text-gray-400"> {{ user.email }} </div>
+							<div class="text-base font-medium leading-none text-white">
+								{{ user.name }}
+							</div>
+							<div class="text-sm font-medium leading-none text-gray-400">
+								{{ user.email }}
+							</div>
 						</div>
 					</div>
 					<div class="mt-3 space-y-1 px-2">
-						<DisclosureButton as="a" @click="logout"
-							class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white cursor-pointer">
-							Logout </DisclosureButton>
+						<DisclosureButton
+							as="a"
+							@click="logout"
+							class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white cursor-pointer"
+						>
+							Logout
+						</DisclosureButton>
 					</div>
 				</div>
 			</DisclosurePanel>
@@ -128,13 +175,11 @@ export default {
 		const router = useRouter();
 
 		function logout() {
-			store
-				.dispatch("logout")
-				.then(() => {
-					router.push({
-						name: "Login",
-					});
+			store.dispatch("logout").then(() => {
+				router.push({
+					name: "Login",
 				});
+			});
 		}
 
 		return {
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,12 +1,44 @@
 import { createStore } from "vuex";
 import axiosClient from "../axios";
 
+const tmpSurveys = [
+	{
+		id: 100,
+		title: "The Codeholic youtube channel content",
+		slug: "thecodeholic-youtube-channel-content",
+		status: "draft",
+		image: "https://picsum.photos/200",
+		description: "My name is Ahmed.<br>I am a Web Developer",
+		created_at: "2022-10-06 18:00:00",
+		updated_at: "2022-10-06 18:00:00",
+		expire_date: "2022-10-08 18:00:00",
+		questions: [
+			{
+				id: 1,
+				type: "select",
+				question: "From which country are you ?",
+				description: null,
+				data: {
+					options: [
+						{
+							uuid: "f8af96f2-1d80-4632-9e9e-b560670e52ea",
+							text: "Egypt",
+						},
+					],
+				},
+			},
+		],
+	},
+];
+
 const store = createStore({
 	state: {
 		user: {
 			data: {},
 			token: sessionStorage.getItem("TOKEN"),
 		},
+		surveys: [...tmpSurveys],
+		questionTypes: ["text", "select", "radio", "checkbox", "textarea"],
 	},
 	getters: {},
 	actions: {
